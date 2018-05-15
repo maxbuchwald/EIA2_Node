ee6cdc7925e9ab2e0ae0b3f7e5efb9cb71326f04
maxbuchwald <32566080+maxbuchwald@users.noreply.github.com>
2018-05-15 17:38:47 +0200

    Add files via upload

--- Server.js
+++ Server.js
@@ -1,32 +1,35 @@
 "use strict";
-const Http = require("http");
-const Url = require("url");
+const Http = require("http"); //HTTP Modul wird eingebunden
+const Url = require("url"); //URL Modul wird eingebunden
 var Server;
 (function (Server) {
-    let port = process.env.PORT;
+    //Interface = Assoziatives Array mit key als SChl�ssel  
+    let port = process.env.PORT; //Globale Variable - representiert den Systemumgebungs-Status der Applikation, wenn sie starte
     if (port == undefined)
         port = 8100;
-    let server = Http.createServer();
-    server.addListener("listening", handleListen);
-    server.addListener("request", handleRequest);
+    let server = Http.createServer(); //erzeugt Server-Objekt, mit dem weiter gearbeitet werden kann
+    server.addListener("listening", handleListen); //wenn Programm l�uft - sprich auf etwas "geh�rt wird", dann Funktionsaufruf von handleListen
+    server.addListener("request", handleRequest); //Server beibringen auf etwas zu h�ren
     server.listen(port);
     function handleListen() {
-        console.log("Ich h�re?");
+        console.log("Ich höre?"); //Konsolenausgabe bzw Terminalausgabe  
     }
     function handleRequest(_request, _response) {
+        console.log("Ich höre Stimmen!");
+        // Ausgabe in der Konsole bzw. im Terminal
+        // Server h�rt, wenn localhost:8100 im Browser ge�ffnet ist;
+        // dann erscheint im Terminal "Ich h�re Stimmen"
+        let query = Url.parse(_request.url, true).query; //umwandeln von /?a=10&b=20 in ein JavaScript-Objekt
+        let a = parseInt(query["a"]); //a wird als Variable definiert
+        let b = parseInt(query["b"]); //b wird als Variable definiert
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        console.log("Ich h�re Stimmen!");
-        _response.write("Ich h�re Stimmen!<br/>");
-        let query = Url.parse(_request.url, true).query;
-        let a = parseInt(query["a"]);
-        let b = parseInt(query["b"]);
-        _response.write("Ich habe dich verstanden.<br/>");
-        for (let key in query) {
-            console.log(query[key]);
-            _response.write("Die eingebene Query-Information ist: " + (query[key]) + "<br/>");
-        }
+        _response.write("Ich habe dich gehört<br/>");
+        for (let key in query)
+            //console.log(query[key]);
+            _response.write("Eingegebene Query-Information lautet: " + (query[key]) + "<br>");
         _response.write("Das Ergebnis ist: " + (a + b));
+        //L�sung erscheint im Browserfenster mit "Das Ergebnis ist:"
         _response.end();
     }
 })(Server || (Server = {}));
--- Server.ts
+++ Server.ts
@@ -1,43 +1,45 @@
-import * as Http from "http";
-import * as Url from "url";
-
+import * as Http from "http"; //HTTP Modul wird eingebunden
+import * as Url from "url"; //URL Modul wird eingebunden
 namespace Server {
     interface AssocStringString {
         [key: string]: string;
-    }
-
-    let port: number = process.env.PORT;
-    if (port == undefined)
+    } //homogenes, assoziatives Array, Daten des Typs string werden dem key zugeordnet
+      //Interface = Assoziatives Array mit key als SChlüssel  
+    
+    let port: number = process.env.PORT; //Globale Variable - representiert den Systemumgebungs-Status der Applikation, wenn sie starte
+    if (port == undefined) //wenn der Port "undefined" ist, dann soll port 8100 sein
         port = 8100;
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("listening", handleListen);
-    server.addListener("request", handleRequest);
+    let server: Http.Server = Http.createServer(); //erzeugt Server-Objekt, mit dem weiter gearbeitet werden kann
+    server.addListener("listening", handleListen); //wenn Programm läuft - sprich auf etwas "gehört wird", dann Funktionsaufruf von handleListen
+    server.addListener("request", handleRequest); //Server beibringen auf etwas zu hören
     server.listen(port);
 
     function handleListen(): void {
-        console.log("Ich höre?");
+        console.log("Ich hÃ¶re?");  //Konsolenausgabe bzw Terminalausgabe  
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //handleRequest hat automatisch zwei Parameter, ohne Rückgabewert
+        console.log("Ich hÃ¶re Stimmen!");
+        // Ausgabe in der Konsole bzw. im Terminal
+        // Server hört, wenn localhost:8100 im Browser geöffnet ist;
+        // dann erscheint im Terminal "Ich höre Stimmen"
+        let query: AssocStringString = Url.parse(_request.url, true).query; //umwandeln von /?a=10&b=20 in ein JavaScript-Objekt
+        let a: number = parseInt(query["a"]); //a wird als Variable definiert
+        let b: number = parseInt(query["b"]); //b wird als Variable definiert
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        console.log("Ich höre Stimmen!");
-        _response.write("Ich höre Stimmen!<br/>");
 
-        let query: AssocStringString = Url.parse(_request.url, true).query;
-        let a: number = parseInt(query["a"]);
-        let b: number = parseInt(query["b"]);
+        _response.write("Ich habe dich gehÃ¶rt<br/>");
         
-        _response.write("Ich habe dich verstanden.<br/>");
-
-        for (let key in query){
-            console.log(query[key]);               
-            _response.write("Die eingebene Query-Information ist: "+ (query[key]) + "<br/>");
-        }
         
+        for (let key in query) 
+            //console.log(query[key]);
+       
+        _response.write("Eingegebene Query-Information lautet: " + (query[key]) + "<br>");
         _response.write("Das Ergebnis ist: " + (a + b));
-
+        //Lösung erscheint im Browserfenster mit "Das Ergebnis ist:"
+        
         _response.end();
     }
 }
