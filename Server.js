a1380de9b52a45d666af0d78a8f508179a48ab66
maxbuchwald <32566080+maxbuchwald@users.noreply.github.com>
2018-05-15 17:21:28 +0200

    Add files via upload

--- Server.js
+++ Server.js
@@ -1,8 +1,8 @@
 "use strict";
 const Http = require("http");
 const Url = require("url");
-var L05_Server;
-(function (L05_Server) {
+var Server;
+(function (Server) {
     let port = process.env.PORT;
     if (port == undefined)
         port = 8100;
@@ -11,20 +11,23 @@ var L05_Server;
     server.addListener("request", handleRequest);
     server.listen(port);
     function handleListen() {
-        console.log("Ich höre?");
+        console.log("Ich h�re?");
     }
     function handleRequest(_request, _response) {
-        console.log("Ich höre Stimmen!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        console.log("Ich h�re Stimmen!");
+        _response.write("Ich h�re Stimmen!<br/>");
         let query = Url.parse(_request.url, true).query;
         let a = parseInt(query["a"]);
         let b = parseInt(query["b"]);
-        for (let key in query)
+        _response.write("Ich habe dich verstanden.<br/>");
+        for (let key in query) {
             console.log(query[key]);
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write("Ich habe dich gehört<br/>");
+            _response.write("Die eingebene Query-Information ist: " + (query[key]) + "<br/>");
+        }
         _response.write("Das Ergebnis ist: " + (a + b));
         _response.end();
     }
-})(L05_Server || (L05_Server = {}));
+})(Server || (Server = {}));
 //# sourceMappingURL=Server.js.map
--- Server.ts
+++ Server.ts
@@ -1,7 +1,7 @@
 import * as Http from "http";
 import * as Url from "url";
 
-namespace L05_Server {
+namespace Server {
     interface AssocStringString {
         [key: string]: string;
     }
@@ -16,23 +16,26 @@ namespace L05_Server {
     server.listen(port);
 
     function handleListen(): void {
-        console.log("Ich hÃ¶re?");
+        console.log("Ich höre?");
     }
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("Ich hÃ¶re Stimmen!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        console.log("Ich höre Stimmen!");
+        _response.write("Ich höre Stimmen!<br/>");
 
         let query: AssocStringString = Url.parse(_request.url, true).query;
         let a: number = parseInt(query["a"]);
         let b: number = parseInt(query["b"]);
+        
+        _response.write("Ich habe dich verstanden.<br/>");
 
-        for (let key in query) 
-            console.log(query[key]);
-
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-
-        _response.write("Ich habe dich gehÃ¶rt<br/>");
+        for (let key in query){
+            console.log(query[key]);               
+            _response.write("Die eingebene Query-Information ist: "+ (query[key]) + "<br/>");
+        }
+        
         _response.write("Das Ergebnis ist: " + (a + b));
 
         _response.end();
