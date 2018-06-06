e2e0b71f12b4aca59198c5834037ddb02fde6d32
maxbuchwald <32566080+maxbuchwald@users.noreply.github.com>
2018-06-06 21:55:19 +0200

    Add files via upload

--- Server.js
+++ Server.js
@@ -1,36 +1,85 @@
 "use strict";
-const Http = require("http"); //HTTP Modul wird eingebunden
-const Url = require("url"); //URL Modul wird eingebunden
+const Http = require("http");
+const Url = require("url");
+// IMPORT HAT BEI MIR NICHT FUNKTIONIERT
 var Server;
 (function (Server) {
-    //Interface = Assoziatives Array mit key als SChl�ssel  
-    let port = process.env.PORT; //Globale Variable - representiert den Systemumgebungs-Status der Applikation, wenn sie starte
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    let studiHomoAssoc = {};
+    let port = process.env.PORT;
     if (port == undefined)
-        port = 8100;
-    let server = Http.createServer(); //erzeugt Server-Objekt, mit dem weiter gearbeitet werden kann
-    server.addListener("listening", handleListen); //wenn Programm l�uft - sprich auf etwas "geh�rt wird", dann Funktionsaufruf von handleListen
-    server.addListener("request", handleRequest); //Server beibringen auf etwas zu h�ren
-    server.listen(port);
-    function handleListen() {
-        console.log("Ich höre?"); //Konsolenausgabe bzw Terminalausgabe  
-    }
-    function handleRequest(_request, _response) {
-        console.log("Ich höre Stimmen!");
-        // Ausgabe in der Konsole bzw. im Terminal
-        // Server h�rt, wenn localhost:8100 im Browser ge�ffnet ist;
-        // dann erscheint im Terminal "Ich h�re Stimmen"
-        let query = Url.parse(_request.url, true).query; //umwandeln von /?a=10&b=20 in ein JavaScript-Objekt
-        let a = parseInt(query["a"]); //a wird als Variable definiert
-        let b = parseInt(query["b"]); //b wird als Variable definiert
+        port = 8200;
+    let server = Http.createServer((_request, _response) => {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write("Ich habe dich gehört<br/>");
-        for (let key in query)
-            //console.log(query[key]);
-            _response.write("Eingegebene Query-Information lautet: " + (query[key]) + "<br>");
-        _response.write("Das Ergebnis ist: " + (a + b));
-        //L�sung erscheint im Browserfenster mit "Das Ergebnis ist:"
+    });
+    server.addListener("request", handleRequest);
+    server.listen(port);
+    function handleRequest(_request, _response) {
+        console.log("Ich h�re Stimmen!");
+        let query = Url.parse(_request.url, true).query;
+        console.log(query["command"]);
+        if (query["command"]) {
+            switch (query["command"]) {
+                case "insert":
+                    insert(query, _response);
+                    break;
+                case "refresh":
+                    refresh(_response);
+                    break;
+                case "search":
+                    search(query, _response);
+                    break;
+                default:
+                    error();
+            }
+        }
         _response.end();
+    }
+    function insert(query, _response) {
+        let obj = JSON.parse(query["data"]);
+        let _name = obj.name;
+        let _firstname = obj.firstname;
+        let matrikel = obj.matrikel.toString();
+        let _age = obj.age;
+        let _gender = obj.gender;
+        let _studiengang = obj.studiengang;
+        let studi;
+        studi = {
+            name: _name,
+            firstname: _firstname,
+            matrikel: parseInt(matrikel),
+            age: _age,
+            gender: _gender,
+            studiengang: _studiengang
+        };
+        studiHomoAssoc[matrikel] = studi;
+        _response.write("Daten empfangen");
+    }
+    function refresh(_response) {
+        console.log(studiHomoAssoc);
+        for (let matrikel in studiHomoAssoc) {
+            let studi = studiHomoAssoc[matrikel];
+            let line = matrikel + ": ";
+            line += studi.studiengang + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)";
+            _response.write(line + "\n");
+        }
+    }
+    function search(query, _response) {
+        let studi = studiHomoAssoc[query["searchFor"]];
+        if (studi) {
+            let line = query["searchFor"] + ": ";
+            line += studi.studiengang + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)";
+            _response.write(line);
+        }
+        else {
+            _response.write("No Match");
+        }
+    }
+    function error() {
+        alert("Error");
     }
 })(Server || (Server = {}));
 //# sourceMappingURL=Server.js.map
--- Server.ts
+++ Server.ts
@@ -1,45 +1,115 @@
-import * as Http from "http"; //HTTP Modul wird eingebunden
-import * as Url from "url"; //URL Modul wird eingebunden
+import * as Http from "http";
+import * as Url from "url";
+
+// IMPORT HAT BEI MIR NICHT FUNKTIONIERT
 namespace Server {
+
     interface AssocStringString {
         [key: string]: string;
-    } //homogenes, assoziatives Array, Daten des Typs string werden dem key zugeordnet
-      //Interface = Assoziatives Array mit key als SChlüssel  
-    
-    let port: number = process.env.PORT; //Globale Variable - representiert den Systemumgebungs-Status der Applikation, wenn sie starte
-    if (port == undefined) //wenn der Port "undefined" ist, dann soll port 8100 sein
-        port = 8100;
-
-    let server: Http.Server = Http.createServer(); //erzeugt Server-Objekt, mit dem weiter gearbeitet werden kann
-    server.addListener("listening", handleListen); //wenn Programm läuft - sprich auf etwas "gehört wird", dann Funktionsaufruf von handleListen
-    server.addListener("request", handleRequest); //Server beibringen auf etwas zu hören
-    server.listen(port);
-
-    function handleListen(): void {
-        console.log("Ich hÃ¶re?");  //Konsolenausgabe bzw Terminalausgabe  
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //handleRequest hat automatisch zwei Parameter, ohne Rückgabewert
-        console.log("Ich hÃ¶re Stimmen!");
-        // Ausgabe in der Konsole bzw. im Terminal
-        // Server hört, wenn localhost:8100 im Browser geöffnet ist;
-        // dann erscheint im Terminal "Ich höre Stimmen"
-        let query: AssocStringString = Url.parse(_request.url, true).query; //umwandeln von /?a=10&b=20 in ein JavaScript-Objekt
-        let a: number = parseInt(query["a"]); //a wird als Variable definiert
-        let b: number = parseInt(query["b"]); //b wird als Variable definiert
+    interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        age: number;
+        gender: boolean;
+        studiengang: string;
+    }
+
+    // Struktur des homogenen assoziativen Arrays, bei dem ein Datensatz der Matrikelnummer zugeordnet ist
+    interface Studis {
+        [matrikel: string]: Studi;
+    }
+    
+
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    let studiHomoAssoc: Studis = {};
+    let port: number = process.env.PORT;
+    if (port == undefined)
+        port = 8200;
+
+    let server: Http.Server = Http.createServer((_request: Http.IncomingMessage, _response: Http.ServerResponse) => {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
+    });
+    server.addListener("request", handleRequest);
+    server.listen(port);
 
-        _response.write("Ich habe dich gehÃ¶rt<br/>");
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("Ich höre Stimmen!");
+        let query: AssocStringString = Url.parse(_request.url, true).query;
+        console.log(query["command"]);
+        if (query["command"] ) {
+            switch (query["command"] ) {
+                case "insert": 
+                    insert(query, _response);
+                    break;
+                 
+                case "refresh":
+                    refresh(_response);
+                    break;
+                    
+                case "search":
+                    search(query, _response);
+                    break;
+               
+                default: 
+                    error();
+            } 
+        }
+        _response.end();    
         
+    }      
         
-        for (let key in query) 
-            //console.log(query[key]);
-       
-        _response.write("Eingegebene Query-Information lautet: " + (query[key]) + "<br>");
-        _response.write("Das Ergebnis ist: " + (a + b));
-        //Lösung erscheint im Browserfenster mit "Das Ergebnis ist:"
+        function insert(query: AssocStringString, _response: Http.ServerResponse): void {
+            let obj: Studi = JSON.parse(query["data"]);
+            let _name: string = obj.name;
+            let _firstname: string = obj.firstname;  
+            let matrikel: string = obj.matrikel.toString(); 
+            let _age: number = obj.age;
+            let _gender: boolean = obj.gender;
+            let _studiengang: string = obj.studiengang;  
+            let studi: Studi;
+            studi = {
+                name: _name,
+                firstname: _firstname,
+                matrikel: parseInt(matrikel),
+                age: _age,
+                gender: _gender,
+                studiengang: _studiengang
+            };  
+            studiHomoAssoc[matrikel] = studi;
+            _response.write("Daten empfangen");
+            }
+
+        function refresh(_response: Http.ServerResponse): void {
+            console.log(studiHomoAssoc);
+            for (let matrikel in studiHomoAssoc) {  
+            let studi: Studi = studiHomoAssoc[matrikel];
+            let line: string = matrikel + ": ";
+            line += studi.studiengang + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)"; 
+            _response.write(line + "\n");                                          
+            }
+        } 
         
-        _response.end();
-    }
+        function search(query: AssocStringString, _response: Http.ServerResponse): void {
+            let studi: Studi = studiHomoAssoc[query["searchFor"]];
+            if (studi) {
+                let line: string = query["searchFor"] + ": ";
+                line += studi.studiengang + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+                line += studi.gender ? "(M)" : "(F)";
+                _response.write(line);
+            } else {
+                _response.write("No Match");    
+            }    
+        }
+        
+        function error(): void {
+            alert("Error"); 
+        }
+
+        
+    
 }
